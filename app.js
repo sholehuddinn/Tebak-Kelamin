0efa786e71f13682ce3d9658d3a1d44dc2e29532
Fajar <fajaruddin.soleh@gmail.com>
2024-06-20 22:32:28 +0700

    membuat tebakan gender

---
+++ app.js
@@ -0,0 +1,24 @@
+const baseApi = "https://api.genderize.io";
+
+
+async function show (name, gender, probability) {
+  const out = document.getElementById('hasil');
+  if (gender == "male") {
+    gender = 'Laki-laki';
+  }else if (gender == "female") {
+    gender = 'Perempuan';
+  }
+  const PredictionText = `Halo ${name}, Jenis Kelamin Kamu Kemungkinan Adalah ${probability * 100}% ${gender}`;
+  out.innerHTML = PredictionText;
+}
+
+async function predict(event) {
+  if (event.key == "Enter") {
+    const firstName = event.target.value;
+    const queryUrl = `${baseApi}/?name=${firstName}`;
+
+    const response = await fetch(queryUrl);
+    const data = await response.json();
+    show(data.name, data.gender, data.probability);
+  }
+}
